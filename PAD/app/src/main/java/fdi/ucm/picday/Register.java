1d7a81fd704260a918c2f4588896b62951c3960d
Dres0109 <andrespl@ucm.es>
2018-06-20 12:53:53 +0200

    Update Register.java

--- PAD/app/src/main/java/fdi/ucm/picday/Register.java
+++ PAD/app/src/main/java/fdi/ucm/picday/Register.java
@@ -9,6 +9,20 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.HashMap;
+import java.util.Map;
+
 public class Register extends Activity {
 
     private Button submit;
@@ -48,22 +62,48 @@ public class Register extends Activity {
     }
 
     private void create_user() {
-        String user_name = inputUserName.getText().toString();
-        String user_email = inputEmail.getText().toString();
-        String user_password = inputPassword.getText().toString();
-        User user = new User(user_name,user_email,user_password);
-        int correct_registration = user.register(this);
-        if (correct_registration == 0) {
-            registration_error.setText("");
-            Intent startIntent = new Intent(Register.this, StartPage.class);
-            startIntent.putExtra(EXTRA_USER,user_name);
-            startActivity(startIntent);
-        }
-        else if (correct_registration == 1) {
-            registration_error.setText("Given username is not available!");
-        }
-        else {
-            registration_error.setText("Password should have at least 6 characters");
+        try {
+            RequestQueue queue = Volley.newRequestQueue(this);
+            String url = "http://192.168.1.40/bdRemota/registroUsuario.php";
+            StringRequest stringRequest = new StringRequest(Request.Method.POST, url,
+                    new Response.Listener<String>() {
+                        @Override
+                        public void onResponse(String response) {
+                            try {
+                                JSONObject obj = new JSONObject(response);
+                                boolean success = obj.getBoolean("success");
+                                if (success) {
+                                    Intent startIntent = new Intent(Register.this, StartPage.class);
+                                    startActivity(startIntent);
+                                } else {
+                                    registration_error.setText("Given username is not available!");
+                                }
+                            }catch(JSONException e){
+                                e.printStackTrace();
+                            }
+                        }
+                    }, new Response.ErrorListener() {
+                @Override
+                public void onErrorResponse(VolleyError error) {
+                    registration_error.setText("Hubo un error al registrar" + error);
+                }
+            }){
+                @Override
+                protected Map<String,String> getParams() throws AuthFailureError {
+                    Map<String,String> params = new HashMap<String,String>();
+                    String name = inputUserName.getText().toString();
+                    String email = inputEmail.getText().toString();
+                    String password = inputPassword.getText().toString();
+                    params.put("name",name);
+                    params.put("email",email);
+                    params.put("password",password);
+                    return params;
+                }
+            };
+
+            queue.add(stringRequest);
+        }catch(Exception e){
+            e.printStackTrace();
         }
     }
 
